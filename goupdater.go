12042c2527ec02f1dac57c9d0f61cea5b357578d
Italo Lelis de Vietro <italolelis@gmail.com>
2018-02-23 12:22:28 +0100

    Accepting context

--- goupdater.go
+++ goupdater.go
@@ -1,6 +1,13 @@
+// Package goupdater helps you to easily build self-updating programs
+//
+// Goupdater makes it easier for you to update your Go programs (or other single-file targets).
+// A program can update itself by replacing its executable file with a new version.
+// It provides the flexibility to implement different updating user experiences like auto-updating,
+// or manual user-initiated updates.
 package goupdater
 
 import (
+	"context"
 	"io"
 
 	"github.com/italolelis/goupdater/updater"
@@ -9,7 +16,7 @@ import (
 
 // Resolver represents an upstream that will be called when checking for a new version
 type Resolver interface {
-	Update(currentVersion string) (io.ReadCloser, error)
+	Update(ctx context.Context, currentVersion string) (io.ReadCloser, error)
 }
 
 type nopCloser struct {
@@ -21,7 +28,12 @@ func (nopCloser) Read(p []byte) (n int, err error) { return 0, nil }
 
 // Update updates the current binary with the chosen resolver
 func Update(resolver Resolver, currentVersion string) (bool, error) {
-	reader, err := resolver.Update(currentVersion)
+	return UpdateWithContext(context.TODO(), resolver, currentVersion)
+}
+
+// UpdateWithContext updates the current binary with the chosen resolver and accepts a context
+func UpdateWithContext(ctx context.Context, resolver Resolver, currentVersion string) (bool, error) {
+	reader, err := resolver.Update(ctx, currentVersion)
 	if err != nil {
 		return false, nil
 	}
